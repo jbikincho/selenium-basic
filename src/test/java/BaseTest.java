58e3b7eef2b86f79a0a4c36bdbbd6aa5cb6c55e0
Lukasz Zbikowski <zbikowski.lukasz#gmail.com>
2021-12-16 19:08:26 +0100

    update junit4 to junit5

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,21 +1,21 @@
 import io.github.bonigarcia.wdm.BrowserManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
     protected static WebDriver driver;
 
-    @BeforeClass
+    @BeforeAll
     public static void setupDriver() {
         BrowserManager instance = WebDriverManager.getInstance(ChromeDriver.class);
         instance.setup();
         driver = new ChromeDriver();
     }
 
-    @AfterClass
+    @AfterAll
     public static void closeDriver() {
         if (driver != null) {
             driver.quit();
